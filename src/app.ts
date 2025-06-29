635de01709c2fd22e918bb294973eb626fb44d7d
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-06-29 16:34:17 +0600

    fixed

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://moto-mart.vercel.app/"],
+    origin: ["http://localhost:5173", "https://moto-mart.vercel.app"],
     credentials: true,
   })
 );
